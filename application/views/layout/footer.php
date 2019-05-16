9ce4ab155f97343240a1df123ea0c500be582301
jcelis <jannmartiecelis13@gmail.com>
2019-05-17 03:50:48 +0800

    Jquery added

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,7 +1,7 @@
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         
-    	<script src="<?php echo base_url('js/bootstrap-4.3.1/jquery-1.10.2.min.js')?>"></script> 
+    	<script src="<?php echo base_url('js/jquery/jquery.js')?>"></script> 
 		<script src="<?php echo base_url('js/bootstrap-4.3.1/bootstrap.min.js')?>"></script>
     </body>
 </html>
